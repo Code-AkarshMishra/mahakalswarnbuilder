97d0010f81f96836478bd9b1af9b2d5dcec13d23
Akarsh Mishra <akarsh1453@gmail.com>
2026-05-22 17:13:28 +0530

    Domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://mahakalswarnbuilder.com'; // Replace with actual domain
+  const baseUrl = 'https://mahakalswarnbuilder.in'; // Replace with actual domain
 
   return [
     {
